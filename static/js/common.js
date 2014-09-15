38dc63f466f136f66ef2b6dbdf8288fdbb415b23
VovanR <mail@vovanr.com>
2014-09-15 21:33:10 +0400

    Fixed require libs problem

--- static/js/common.js
+++ static/js/common.js
@@ -2,8 +2,12 @@
  * @author VovanR [mail@vovanr.com]
  */
 
+if (!requirejs.config.baseUrl) {
+    requirejs.config.baseUrl = '../js/';
+}
+
 requirejs.config({
-    baseUrl: '../js/',
+    baseUrl: requirejs.config.baseUrl,
     paths: {
         jquery: '../vendor/jquery/dist/jquery',
     },
